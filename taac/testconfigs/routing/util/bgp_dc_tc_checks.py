ee818d83ffdbb4ac9c0c5c66e5d4b1123e1f728e
deepakgudiseva-nexthop <deepakgudiseva@nexthop.ai>
2026-09-18 13:25:14 -0700

    one 6 GiB memory ceiling for every service except bgpd
    Signed-off-by: deepakgudiseva-nexthop <deepakgudiseva@nexthop.ai>

--- taac/testconfigs/routing/util/bgp_dc_tc_checks.py
+++ taac/testconfigs/routing/util/bgp_dc_tc_checks.py
@@ -194,14 +194,8 @@ def build_bgp_dc_tc_prechecks(prefix_limit, *, include_traffic_check, device_nam
         create_prefix_limit_check(prefix_limit=prefix_limit),
         create_unclean_exit_check(),
         create_memory_utilization_check(
-            threshold=5 * (1024**3),
-            threshold_by_service={
-                "bgpd": 4.5 * (1024**3),
-                "fsdb": 7 * (1024**3),
-                "qsfp_service": 2 * (1024**3),
-                "fboss_sw_agent": 12 * (1024**3),
-                "fboss_hw_agent@0": 8 * (1024**3),
-            },
+            threshold=6 * (1024**3),
+            threshold_by_service={"bgpd": 4.5 * (1024**3)},
             start_time_jq_var="test_case_start_time",
         ),
         BGP_SESSION_HEALTHCHECK_NO_V6_LOSS_EXPECTED,
@@ -231,14 +225,8 @@ def build_bgp_dc_tc_postchecks(
         BGP_SESSION_HEALTHCHECK_NO_V6_LOSS_EXPECTED,
         create_unclean_exit_check(),
         create_memory_utilization_check(
-            threshold=5 * (1024**3),
-            threshold_by_service={
-                "bgpd": 4.5 * (1024**3),
-                "fsdb": 5 * (1024**3),
-                "qsfp_service": 2 * (1024**3),
-                "fboss_sw_agent": 12 * (1024**3),
-                "fboss_hw_agent@0": 8 * (1024**3),
-            },
+            threshold=6 * (1024**3),
+            threshold_by_service={"bgpd": 4.5 * (1024**3)},
             start_time_jq_var="test_case_start_time",
         ),
         create_cpu_utilization_check(
